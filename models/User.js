68f5d004ba2953be1ff8106d5860e7f19bb1fd55
angelgiovanna <aescobar2045@gmail.com>
2022-04-27 21:23:37 -0400

    .

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create our user model
+class User extends Model {}
+
+//define table columns and configuration
+User.init(
+    {
+        id: {
+            // use the special Sequelize DataTypes object provide what type of data it is 
+            type: DataTypes.INTEGER,
+            // this is equivalent of SQL's `NOT NULL` option
+            allowNull: false,
+            // instruct that this is the Primary Key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            //there cannot be any duplicate email values in this table
+            unique: true,
+            // if allowNull is set to false, we can run our data through validators before creating the table data
+            validate: {
+                isEmail: true
+            }
+        },
+        // define a passord column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // this means the password must be at least four characters long
+                len: [4]
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
